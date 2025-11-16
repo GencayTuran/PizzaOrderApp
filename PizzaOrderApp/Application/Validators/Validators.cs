470be88e8938b1d68f0ce414298f4dbc43c21bf5
Gencay <gencay.turan@hotmail.com>
2025-11-17 00:54:31 +0100

    introduced validation of dto's using FluentValidation

---
+++ PizzaOrderApp/Application/Validators/Validators.cs
@@ -0,0 +1,45 @@
+﻿using Application.Data;
+using FluentValidation;
+
+namespace Application.Validators
+{
+    public class ClientValidator : AbstractValidator<ClientDto>
+    {
+        public ClientValidator() 
+        {
+            RuleFor(client => client.FirstName).NotEmpty();
+            RuleFor(client => client.LastName).NotEmpty();
+            RuleFor(client => client.PhoneNumber).NotEmpty();
+            RuleFor(client => client.EmailAddress).NotEmpty();
+        }
+    }
+
+    public class DeliveryValidator : AbstractValidator<DeliveryDto> 
+    {
+        public DeliveryValidator()
+        {
+            RuleFor(d => d.DeliveryType).NotEmpty();
+            RuleFor(d => d.DeliveryDateTime).GreaterThanOrEqualTo(DateTime.Now);
+        }
+    }
+
+    public class PizzaValidator : AbstractValidator<PizzaDto>
+    {
+        public PizzaValidator()
+        {
+            RuleFor(p => p.PizzaType).NotEmpty();
+            RuleFor(p => p.PizzaSize).NotEmpty();
+        }
+    }
+
+    public class PizzaOrderValidator : AbstractValidator<PizzaOrderDto>
+    {
+        public PizzaOrderValidator()
+        {
+            RuleFor(p => p.Client).NotEmpty();
+            RuleFor(p => p.Delivery).NotEmpty();
+            RuleFor(p => p.Pizzas).NotEmpty();
+        }
+    }
+
+}
